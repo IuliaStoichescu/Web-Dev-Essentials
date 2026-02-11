0e09bcc6d5db6d7b526c8a25074e527c03ee2fd4
iuli <iulia.stoichescu25@gmail.com>
2026-02-11 17:58:02 +0200

    Backend part

---
+++ Templating/EJX/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');// Set the view engine to EJS
+app.set('views', path.join(__dirname, '/views'));// Set the directory for EJS templates
+app.get('/', (req, res) => {
+   res.render('home');
+})
+
+app.listen(3000, () => {
+   console.log('Server is running on port 3000');
+});
